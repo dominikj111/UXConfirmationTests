350b1bd7a8cc9d658f93add18e3822fa99a5aa49
Dominik Jelinek <dominik.jelinek@any-3.com>
2023-11-15 04:56:17 +0100

    Fix multiselect with footer unit tests

--- src/multiselect_with_footer_template/MyMultiSelect.jsx
+++ src/multiselect_with_footer_template/MyMultiSelect.jsx
@@ -3,16 +3,16 @@ import { MultiSelect } from "primereact/multiselect";
 import FooterTemplate from "./FooterTemplate";
 
 const options = [
-  { label: "Option 1", value: "Option 1" },
-  { label: "Option 2", value: "Option 2" },
-  { label: "Option 3", value: "Option 3" },
-  { label: "Option 4", value: "Option 4" },
-  { label: "Option 5", value: "Option 5" },
-  { label: "Option 6", value: "Option 6" },
-  { label: "Option 7", value: "Option 7" },
-  { label: "Option 8", value: "Option 8" },
-  { label: "Option 9", value: "Option 9" },
-  { label: "Option 10", value: "Option 10" },
+  { label: "Option 1", value: 1 },
+  { label: "Option 2", value: 2 },
+  { label: "Option 3", value: 3 },
+  { label: "Option 4", value: 4 },
+  { label: "Option 5", value: 5 },
+  { label: "Option 6", value: 6 },
+  { label: "Option 7", value: 7 },
+  { label: "Option 8", value: 8 },
+  { label: "Option 9", value: 9 },
+  { label: "Option 10", value: 10 },
 ];
 
 export default function App({ onChange, placeholder }) {
--- src/multiselect_with_footer_template/confirmation.test.jsx
+++ src/multiselect_with_footer_template/confirmation.test.jsx
@@ -1,95 +1,55 @@
 import React from "react";
-import {
-  render,
-  screen,
-  fireEvent,
-  act,
-  waitFor,
-} from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { render, screen, fireEvent } from "@testing-library/react";
 
 import MyMultiSelect from "./MyMultiSelect";
 
-// describe("These tests cover MyMultiSelect with expanded dropdown panel", () => {
-//   let sut;
-//   let user;
-//   let onChangeValue;
+describe("These tests cover MyMultiSelect expanding dropdown panel", () => {
+  let onChangeValue;
 
-//   beforeEach(async () => {
-//     onChangeValue = jest.fn();
-//     user = userEvent.setup();
-
-//     sut = render(
-//       <MyMultiSelect placeholder="placeholder345" onChange={onChangeValue} />
-//     );
-
-//     // await waitFor(async () =>
-
-//     const dropdownLabel = await sut.getByText(/placeholder345/i);
-
-//       console.log(sut.container.querySelector(".p-multiselect"));
-
-
-//     // await act(async () => {
-//       // await fireEvent.click(sut.container.querySelector(".p-multiselect"));
-//       await user.click(sut.container.querySelector(".p-multiselect"));
-//       // await fireEvent.animationEnd(dropdownLabel)
-//     // });
-
-//     console.log(sut.container.outerHTML);
-
-//     // );
-//   });
-
-//   // it("Should not call onChange yet", () => {
-//   //   expect(onChangeValue).not.toHaveBeenCalled();
-//   // });
-
-//   // it("Has available buttons to save and cancel", () => {
-//   //   expect(screen.findByText(/save/i)).not.toBeNull();
-//   //   expect(screen.findByText(/cancel/i)).not.toBeNull();
-//   // });
-
-//   it("Trigger onChange when save or cancel is clicked", async () => {
-//     //   // fireEvent.click(await screen.findByText(/save/i));
-
-//     // console.log((await screen.findAllByRole("button")).length);
-//     //   fireEvent.click(await screen.findByText(/cancel/i));
-
-//     //   expect(onChangeValue).toHaveBeenCalledTimes(2);
-//     expect(1).toBe(1);
-//   });
-// });
-
-describe("Test MyMultiSelect component", () => {
-  it("Clicking on the component shows the dropdown panel with buttons", async () => {
-    const onChangeValue = jest.fn();
-    const user = userEvent.setup();
-
-    const { container } = render(
-      <MyMultiSelect placeholder="placeholder345" onChange={onChangeValue} />
+  beforeEach(() => {
+    onChangeValue = jest.fn();
+    render(
+      <MyMultiSelect placeholder="Select an Option" onChange={onChangeValue} />
     );
+  });
 
-    const multiSelectComponent = await screen.getByText(/placeholder345/i);
-    const trigger = await container.querySelector(".p-multiselect-trigger");
+  it("Has available options and footer buttons after click on the placeholder", () => {
+    expect(screen.queryByText(/save/i)).toBeNull();
+    expect(screen.queryByText(/cancel/i)).toBeNull();
+    expect(screen.queryByText(/option 5/i)).toBeNull();
 
-    console.log(multiSelectComponent.outerHTML);
-    console.log(trigger.outerHTML);
+    fireEvent.click(screen.queryByText(/select an option/i));
 
-    // Click on the component to show the dropdown panel
-    // fireEvent.click(multiSelectComponent);
-    // fireEvent.click(trigger);
+    expect(screen.queryByText(/save/i)).not.toBeNull();
+    expect(screen.queryByText(/cancel/i)).not.toBeNull();
+    expect(screen.queryByText(/option 5/i)).not.toBeNull();
+  });
+});
+
+describe("These tests cover MyMultiSelect with expanded dropdown panel", () => {
+  let onChangeValue;
+
+  beforeEach(() => {
+    onChangeValue = jest.fn();
+    render(
+      <MyMultiSelect placeholder="Select an Option" onChange={onChangeValue} />
+    );
+    fireEvent.click(screen.queryByText(/select an option/i));
+  });
+
+  it("Should not call onChange yet", () => {
+    expect(onChangeValue).not.toHaveBeenCalled();
+  });
+
+  it("Trigger onChange when save or cancel is clicked", () => {
+    fireEvent.click(screen.queryByText(/option 5/i));
+    fireEvent.click(screen.queryByText(/option 4/i));
+    fireEvent.click(screen.queryByText(/option 3/i));
+
+    fireEvent.click(screen.queryByText(/save/i));
     
-    waitFor(async () => await user.click(trigger));
+    fireEvent.click(screen.queryByText(/cancel/i));
 
-
-    // Confirm the existence of buttons in the dropdown panel
-    const saveButton = await screen.findByLabelText("Save");
-    const cancelButton = await screen.findByLabelText("Cancel");
-
-    expect(saveButton).not.toBeNull();
-    expect(cancelButton).not.toBeNull();
-
-    console.log(container.outerHTML);
+    expect(onChangeValue).toHaveBeenCalledWith([]);
   });
 });
